5b3f5e654200864d60d20ce76ee34e68aa821b7a
l0ganc <jingyige@usc.edu>
2019-06-18 17:32:49 -0700

    LC127 Word Ladder

---
+++ src/main/java/Day3/LC127WordLadder.java
@@ -0,0 +1,93 @@
+package Day3;
+
+import java.util.*;
+
+public class LC127WordLadder {
+    /**
+     * Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+     *
+     * Only one letter can be changed at a time.
+     * Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+     * Note:
+     *
+     * Return 0 if there is no such transformation sequence.
+     * All words have the same length.
+     * All words contain only lowercase alphabetic characters.
+     * You may assume no duplicates in the word list.
+     * You may assume beginWord and endWord are non-empty and are not the same.
+     * Example 1:
+     *
+     * Input:
+     * beginWord = "hit",
+     * endWord = "cog",
+     * wordList = ["hot","dot","dog","lot","log","cog"]
+     *
+     * Output: 5
+     *
+     * Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+     * return its length 5.
+     * Example 2:
+     *
+     * Input:
+     * beginWord = "hit"
+     * endWord = "cog"
+     * wordList = ["hot","dot","dog","lot","log"]
+     *
+     * Output: 0
+     *
+     * Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+     */
+
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+        if (set.contains(beginWord)) {
+            set.remove(beginWord);
+        }
+
+        int step = 1;
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                char[] cur = queue.poll().toCharArray();
+
+                for (int j = 0; j < cur.length; j++) {
+                    char temp = cur[j];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        cur[j] = c;
+                        String newStr = new String(cur);
+                        if (set.contains(newStr)) {
+                            if (newStr.equals(endWord)) {
+                                return step + 1;
+                            }
+                            queue.add(newStr);
+                            set.remove(newStr);
+                        }
+                    }
+
+                    cur[j] = temp;
+                }
+            }
+
+            step++;
+        }
+
+        return step;
+
+
+    }
+
+    public static void main(String[] args) {
+        List<String> wordList1 = Arrays.asList("hot","dot","dog","lot","log","cog");
+        List<String> wordList2 = Arrays.asList("hot","dot","dog","lot","log");
+        System.out.println(ladderLength("hit", "cog", wordList1));
+        System.out.println(ladderLength("hit", "cog", wordList2));
+    }
+}
