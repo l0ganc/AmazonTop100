9b884d998b8489727f87d79b41a887a2564e3b3d
l0ganc <jingyige@usc.edu>
2019-06-18 19:42:31 -0700

    LC139 Word Break

---
+++ src/main/java/Day3/LC139WordBreak.java
@@ -0,0 +1,58 @@
+package Day3;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class LC139WordBreak {
+    /**
+     * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+     *
+     * Note:
+     *
+     * The same word in the dictionary may be reused multiple times in the segmentation.
+     * You may assume the dictionary does not contain duplicate words.
+     * Example 1:
+     *
+     * Input: s = "leetcode", wordDict = ["leet", "code"]
+     * Output: true
+     * Explanation: Return true because "leetcode" can be segmented as "leet code".
+     * Example 2:
+     *
+     * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+     * Output: true
+     * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+     *              Note that you are allowed to reuse a dictionary word.
+     * Example 3:
+     *
+     * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+     * Output: false
+     *
+     */
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        // time = O(n^2)
+        // space = O(n)
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        HashSet<String> set = new HashSet<>(wordDict);
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordBreak("leetcode", Arrays.asList("leet", "code")));
+        System.out.println(wordBreak("applepenapple", Arrays.asList("apple", "pen")));
+        System.out.println(wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+    }
+}
